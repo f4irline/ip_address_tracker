3dae8cb4712d07f88177fdcaf43f50a0ac7b1963
Tommi Lepola <lepola.tommi@gmail.com>
2021-10-09 23:16:43 +0300

    Add animations to info card

--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -8,75 +8,79 @@ class InfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return locationInfo != null
-        ? SizedBox(
-            width: double.infinity,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        'IP Address'.toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyText2,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        locationInfo!.ip,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        'Location'.toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyText2,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        '${locationInfo!.location.city}, ${locationInfo!.location.region}'
-                        '${locationInfo!.location.postalCode.isNotEmpty ? ', ${locationInfo!.location.postalCode}' : ''}',
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        'Timezone'.toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyText2,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        locationInfo!.location.timezone,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        'ISP'.toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyText2,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        locationInfo!.as != null &&
-                                locationInfo!.as!.name.isNotEmpty
-                            ? locationInfo!.as!.name
-                            : locationInfo!.isp,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                ]
+    return SizedBox(
+      width: double.infinity,
+      child: AnimatedScale(
+        duration: const Duration(milliseconds: 300),
+        scale: locationInfo == null ? 0 : 1,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: locationInfo == null
+                ? []
+                : [
+                    Column(
+                      children: [
+                        Text(
+                          'IP Address'.toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          locationInfo!.ip,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Location'.toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          '${locationInfo!.location.city}, ${locationInfo!.location.region}'
+                          '${locationInfo!.location.postalCode.isNotEmpty ? ', ${locationInfo!.location.postalCode}' : ''}',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Timezone'.toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          locationInfo!.location.timezone,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'ISP'.toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          locationInfo!.as != null &&
+                                  locationInfo!.as!.name.isNotEmpty
+                              ? locationInfo!.as!.name
+                              : locationInfo!.isp,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                  ]
                     .map(
                       (col) => Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
@@ -84,9 +88,9 @@ class InfoCard extends StatelessWidget {
                       ),
                     )
                     .toList(),
-              ),
-            ),
-          )
-        : const SizedBox.shrink();
+          ),
+        ),
+      ),
+    );
   }
 }
